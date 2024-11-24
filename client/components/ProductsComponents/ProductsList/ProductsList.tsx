8e86b9d306d0b8e32725cf2484417551a4183bd2
separatrixxx <nikitalochmatov@gmail.com>
2024-11-24 15:27:17 +0300

    update products list

--- client/components/ProductsComponents/ProductsList/ProductsList.tsx
+++ client/components/ProductsComponents/ProductsList/ProductsList.tsx
@@ -2,16 +2,17 @@ import styles from './ProductsList.module.css';
 import { useSetup } from '../../../hooks/useSetup';
 import { ProductItem } from '../ProductItem/ProductItem';
 import { Spinner } from '../../Common/Spinner/Spinner';
+import { Htag } from '../../Common/Htag/Htag';
+import { setLocale } from '../../../helpers/locale.helper';
 
 
 export const ProductsList = (): JSX.Element => {
-    const { products } = useSetup();
-    console.log(products)
+    const { router, products } = useSetup();
 
     return (
         <>
             {
-                products.total_count > -1 ?
+                products.total_count > 0 ?
                     <div className={styles.productsList}>
                         {products.products.map(p => 
                             <ProductItem key={p.id} name={p.name}
@@ -19,6 +20,10 @@ export const ProductsList = (): JSX.Element => {
                                 photo={p.photo} type={p.type} />
                         )}
                     </div>
+                : products.total_count === 0 ?
+                    <Htag tag='l' className={styles.notFoundText}>
+                        {setLocale(router.locale).no_products_found}
+                    </Htag>
                 :
                     <div className={styles.spinnerDiv}>
                         <Spinner />
